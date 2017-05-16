a6859aaf9d2b7132b20058a59441cd18a6ab87cc
Ricky Capon <rickycapon@jemmas-mbp.home>
2017-05-16 18:30:17 +0100

    failed first commit

---
+++ capon social/signInvc.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  capon social
+//
+//  Created by Ricky Capon on 16/05/2017.
+//  Copyright © 2017 Ricky Capon. All rights reserved.
+//
+
+import UIKit
+
+class signInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
